7134516142d98cd8d28df3b0d303f9e439f67424
joeshithaapandian <154114850+joeshithaapandian@users.noreply.github.com>
2024-12-04 08:50:53 +0530

    Update exph.py

--- exph.py
+++ exph.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello world"
 
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
